436500ac0bbeb756f8fe287a31eb5cf196814bf0
nutnaik <nutnaik@cisco.com>
2016-12-06 22:28:36 +0530

    Assignment 8.4

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+	//document.getElementById("#tutoriallink").innerHTML ="from JQuery";
+	$("#tutoriallink").attr("href", "http://www.google.com/");
+	$("#tutoriallink").text("from jquery");
+
+});
